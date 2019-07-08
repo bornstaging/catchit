924c0116b4d7373b005a643fe14693a857253575
pachivarnag <pachivarnag@gmail.com>
2019-07-08 14:59:59 -0400

    updates

--- Trace_Big_B/js/lib.js
+++ Trace_Big_B/js/lib.js
@@ -8,7 +8,17 @@ var pixels = null;
 var letterpixels = null;
 var count = ["images/B-Writing.gif","images/B-Writing.gif","images/B-Writing.gif"];
 var i = 0;
-var gifX, gifY
+var gifX, gifY;
+var OSName = "Unknown";
+if (window.navigator.userAgent.indexOf("Windows NT 10.0")!= -1) OSName="Windows 10";
+if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1) OSName="Windows 8";
+if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1) OSName="Windows 7";
+if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1) OSName="Windows Vista";
+if (window.navigator.userAgent.indexOf("Windows NT 5.1") != -1) OSName="Windows XP";
+if (window.navigator.userAgent.indexOf("Windows NT 5.0") != -1) OSName="Windows 2000";
+if (window.navigator.userAgent.indexOf("Mac")            != -1) OSName="Mac/iOS";
+if (window.navigator.userAgent.indexOf("X11")            != -1) OSName="UNIX";
+if (window.navigator.userAgent.indexOf("Linux")          != -1) OSName="Linux";
 
 $(".pyro").hide();
 
@@ -137,7 +147,7 @@ function drawletter(letter) {
   }else{
     $('.bGif').css('left',centerx+20+'px').css('top',centery-(fontMeasurement.height)+30+'px').css('width',cx.measureText(letter).width-40+'px');
   }
-};
+}
 
 
 
