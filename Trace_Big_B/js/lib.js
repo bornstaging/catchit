67a953b307f30102b54528c330ac5146e1b94253
pachivarnag <pachivarnag@gmail.com>
2019-07-08 15:30:05 -0400

    updates

--- Trace_Big_B/js/lib.js
+++ Trace_Big_B/js/lib.js
@@ -142,7 +142,9 @@ function drawletter(letter) {
   cx.fillText(letter, centerx, centery);
   //console.log(fontMeasurement.height+40)
   if(OSName == "Mac/iOS"){
-    console.log($(window).width());
+   // console.log($(window).width());
+    $('.bGif').css('left',centerx+23+'px').css('top',centery-(fontMeasurement.height)+55+'px').css('width',cx.measureText(letter).width-45+'px');
+  }else if(OSName == "Linux"){
     $('.bGif').css('left',centerx+23+'px').css('top',centery-(fontMeasurement.height)+55+'px').css('width',cx.measureText(letter).width-45+'px');
   }else{
     $('.bGif').css('left',centerx+20+'px').css('top',centery-(fontMeasurement.height)+30+'px').css('width',cx.measureText(letter).width-45+'px');
